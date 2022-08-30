51d7283f92f4b983eadad1b64865c54ab0ab8e92
Jyguy <jskekkx@gmail.com>
2022-08-29 21:15:04 -0400

    chore: branding and license

--- src/main/java/xyz/reknown/skycarrier/SkyCarrier.java
+++ src/main/java/xyz/reknown/skycarrier/SkyCarrier.java
@@ -1,14 +1,14 @@
-package net.fabricmc.example;
+package xyz.reknown.skycarrier;
 
 import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class SkyCarrier implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("skycarrier");
 
 	@Override
 	public void onInitialize() {
